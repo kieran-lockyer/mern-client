eb8493211acd23d5eccc67c96219a3501bc02948
Glenn Francis <XglennfrancisX@gmail.com>
2019-01-20 17:26:59 +1000

    optimized responsive styles

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -13,60 +13,61 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
-        <section className="featured">
-          <div className="featured-graph chart">
-            <DemoChart />
-          </div>
-          <div className="featured-stats">
-            <div className="featured-stats-item">
-              <h2>
-                <AnimatedNumber
-                  value={3304}
-                  style={{
-                    transition: "0.8s ease-out",
-                    transitionProperty: "background-color, color, opacity"
-                  }}
-                  formatValue={n => this.numberWithCommas(n)}
-                  stepPrecision={0}
-                  duration={1000}
-                />
-              </h2>
-              <span>Photos</span>
+        <div className="featured-container">
+          <section className="featured">
+            <div className="featured-graph chart">
+              <DemoChart />
             </div>
-            <div className="featured-stats-item">
-              <h2>
-                <AnimatedNumber
-                  value={1700}
-                  style={{
-                    transition: "0.8s ease-out",
-                    transitionProperty: "background-color, color, opacity"
-                  }}
-                  formatValue={n => this.numberWithCommas(n)}
-                  stepPrecision={0}
-                  duration={1000}
-                />
-              </h2>
-              <span>Tags</span>
+            <div className="featured-stats">
+              <div className="featured-stats-item">
+                <h2>
+                  <AnimatedNumber
+                    value={3304}
+                    style={{
+                      transition: "0.8s ease-out",
+                      transitionProperty: "background-color, color, opacity"
+                    }}
+                    formatValue={n => this.numberWithCommas(n)}
+                    stepPrecision={0}
+                    duration={1000}
+                  />
+                </h2>
+                <span>Photos</span>
+              </div>
+              <div className="featured-stats-item">
+                <h2>
+                  <AnimatedNumber
+                    value={1700}
+                    style={{
+                      transition: "0.8s ease-out",
+                      transitionProperty: "background-color, color, opacity"
+                    }}
+                    formatValue={n => this.numberWithCommas(n)}
+                    stepPrecision={0}
+                    duration={1000}
+                  />
+                </h2>
+                <span>Tags</span>
+              </div>
+              <div className="featured-stats-item">
+                <h2>
+                  <AnimatedNumber
+                    value={15}
+                    style={{
+                      transition: "0.8s ease-out",
+                      transitionProperty: "background-color, color, opacity"
+                    }}
+                    formatValue={n => this.numberWithCommas(n)}
+                    stepPrecision={0}
+                    duration={1000}
+                  />
+                </h2>
+                <span>Flagged</span>
+              </div>
             </div>
-            <div className="featured-stats-item">
-              <h2>
-                <AnimatedNumber
-                  value={15}
-                  style={{
-                    transition: "0.8s ease-out",
-                    transitionProperty: "background-color, color, opacity"
-                  }}
-                  formatValue={n => this.numberWithCommas(n)}
-                  stepPrecision={0}
-                  duration={1000}
-                />
-              </h2>
-              <span>Flagged</span>
-            </div>
-          </div>
-        </section>
-        {/* <Analytics /> */}
-        <SortableComponent />
+          </section>
+          <SortableComponent />
+        </div>
       </div>
     );
   }
