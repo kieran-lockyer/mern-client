d71139b384c6df45a2564bd41e3bab5354f45be3
Glenn Francis <XglennfrancisX@gmail.com>
2019-01-24 11:42:17 +1000

    updated the baseurl for request

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001"
-  // baseURL: "https://ca-mern-server.herokuapp.com"
+  baseURL: "https://ca-mern-server.herokuapp.com"
 });
