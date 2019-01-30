ae81a87afae72dc4424939f73d70e622fa4bf1b5
kieran-lockyer <kieran.lockyer.87@gmail.com>
2019-01-30 20:56:16 +1000

    implemented further dashboard stats such as trending, commong, and avg new data

--- src/components/Dashboard/AnalyticsCard.tsx
+++ src/components/Dashboard/AnalyticsCard.tsx
@@ -1,20 +1,51 @@
 import React, { Component } from "react";
 import AnalyticsItem from "./AnalyticsItem";
 import { Card, CardHeading } from "../../styles/AppStyles";
+import api from '../../api/index'
 
 export default class AnalyticsCard extends Component<any, any> {
-  renderTags() {
-    return this.props.tags.map((tag, id) => {
-      return <AnalyticsItem tag={tag} key={id} />;
-    });
+  state = {
+    poptags: [],
+    trendingtags: [],
   }
+
+  componentDidMount() {
+    api.get('/tags/stats/get/poptags').then(res => {
+      this.setState({
+        poptags: res.data
+      })
+    })
+
+    api.get('/tags/stats/get/trendingtags').then(res => {
+      this.setState({
+        trendingtags: res.data
+      })
+    })
+  }
+
+  renderPopTags = () => {
+    return this.state.poptags.map(item => {
+      return <AnalyticsItem tag={item._id} count={item.count} key={item._id} />;
+    })
+  }
+
+  renderTrendingTags = () => {
+    return this.state.trendingtags.map(item => {
+      return <AnalyticsItem tag={item._id} count={item.count} key={item._id} />;
+    })
+  }
+
+  renderCard = () => {
+    return this.props.title === "Most Popular Tags" ? this.renderPopTags() : this.renderTrendingTags()
+  }
+
   render() {
     return (
       <Card>
         <CardHeading>
           <h3>{this.props.title}</h3>
         </CardHeading>
-        {this.renderTags()}
+        {this.renderCard()}
       </Card>
     );
   }
