6f0db890f32e2d06c785573c9372bbfa83b9f21e
Glenn Francis <XglennfrancisX@gmail.com>
2019-01-31 13:21:25 +1000

    integrated sorting for tags, setup table format

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -2,31 +2,21 @@ import api from "../api";
 import * as type from "./types";
 
 // Fetch List of Tags
-export const fetchTags = pageNum => async dispatch => {
-  const response = await api.get(`/tags?pageNo=${pageNum}`);
+export const fetchTags = (
+  pageNum,
+  limit = 30,
+  field = "dateAdded",
+  order = "desc",
+  label = ""
+) => async dispatch => {
+  const response = await api.get(
+    `tags?pageNo=${pageNum}&limit=${limit}&field=${field}&order=${order}&label=${label}`
+  );
   dispatch({ type: type.FETCH_TAGS, payload: response.data });
 };
 
-// Filter photos
-export const filterPhotos = tags => async dispatch => {
-  const response = await api.get(`/photos/tag/${tags}`);
-  dispatch({ type: type.FILTER_PHOTOS, payload: response.data });
-};
-
-// Fetch images attached to specific tag
-export const fetchImages = tag => async dispatch => {
-  const response = await api.get(`/tags/images/${tag}`);
-  dispatch({ type: type.FETCH_TAG_IMAGES, payload: response.data });
-};
-
 // Fetch List of Photos
-export const fetchPhotos = pageNum => async dispatch => {
-  const response = await api.get(`/photos?pageNo=${pageNum}`);
-  dispatch({ type: type.FETCH_PHOTOS, payload: response.data });
-};
-
-// Fetch List of Photos
-export const sortPhotos = (
+export const fetchPhotos = (
   pageNum,
   limit = 30,
   field = "dateAdded",
@@ -37,4 +27,10 @@ export const sortPhotos = (
     `photos?pageNo=${pageNum}&limit=${limit}&field=${field}&order=${order}&tags=${tags}`
   );
   dispatch({ type: "SORT_PHOTOS", payload: response.data });
+};
+
+// Fetch images attached to specific tag
+export const fetchTagPhotos = tag => async dispatch => {
+  const response = await api.get(`/tags/images/${tag}`);
+  dispatch({ type: type.FETCH_TAG_IMAGES, payload: response.data });
 };
