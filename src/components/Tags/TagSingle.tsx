8962259d7f3e4cbd9d6a90c1199c4f562966a9d9
Glenn Francis <XglennfrancisX@gmail.com>
2019-02-02 18:01:22 +1000

    added tag single details, formatted the details

--- src/components/Tags/TagSingle.tsx
+++ src/components/Tags/TagSingle.tsx
@@ -3,70 +3,51 @@ import * as Tags from "../../styles/AppStyles";
 import * as actions from "../../actions";
 import { connect } from "react-redux";
 import baseUrl from "../../api/baseurl";
-import Gallery from "react-grid-gallery";
-import Moment from "moment";
-
+import moment from "moment";
 class TagSingle extends React.Component<any, any> {
   componentDidMount() {
     this.props.fetchTagPhotos(this.props.match.params.tagname);
-    console.log(this.props);
-  }
-
-  renderImages() {
-    return this.props.images.map(tag => {
-      return (
-        <img
-          src={baseUrl + "/photos/image/" + tag.imageId}
-          alt="image"
-          width="40%"
-          height="40%"
-        />
-      );
-    });
   }
 
   public render() {
     const images = [];
     this.props.images.forEach(function(tag) {
       images.push({
+        title: tag.label,
         src: baseUrl + "/photos/image/" + tag.imageId,
         thumbnail: baseUrl + "/photos/image/" + tag.imageId,
-        thumbnailWidth: "60%",
-        thumbnailHeight: "60%",
-        caption: `
-          Model: ${tag.source.model} |
-          Confidence: ${(tag.confidence * 100).toFixed(2)} |
-          Date: ${tag.dateAdded}
-        `,
+        thumbnailWidth: 3000,
+        thumbnailHeight: 2000,
+        caption: [
+          "Model: " + tag.source.model,
+          "Confidence: " + (tag.confidence * 100).toFixed(2),
+          "Date: " + tag.dateAdded
+        ],
         tags: [{ value: tag.source.type }]
       });
     });
-
-    const tagStyles = {
-      background: "#5c7080",
-      color: "#fff",
-      padding: " 2px 5px",
-      fontSize: "12px",
-      borderRadius: "5px",
-      display: "inline-block"
-    };
 
     return (
       <Tags.Container>
         <Tags.Wrapper>
           <Tags.Header>
-            <h2 style={{ color: "white" }}>
-              {this.props.match.params.tagname}
-            </h2>
+            <h2 style={{ color: "white" }}>{images[0] && images[0].title}</h2>
           </Tags.Header>
-          <div>
-            {images[0] && (
-              <Gallery
-                images={images}
-                backdropClosesModal
-                tagStyle={tagStyles}
-              />
-            )}
+          <div className="tag-block">
+            <div className="tag-img">
+              {images[0] && (
+                <img
+                  src={`${images[0].src}`}
+                  alt=""
+                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                />
+              )}
+            </div>
+            <div className="tag-details">
+              <p>{images[0] && images[0].caption[0]}</p>
+              <p>{images[0] && images[0].caption[1]}</p>
+              <p>{images[0] && images[0].caption[2]}</p>
+            </div>
           </div>
         </Tags.Wrapper>
       </Tags.Container>
